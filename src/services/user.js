48f69c01ca9ec8841e116b5c5191455b77bb74b0
SophiaSantos17 <santosvsophia17@gmail.com>
2024-02-16 22:19:32 -0300

    Feat: conexão com api de teste funcionando

---
+++ src/services/user.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+import { useAuth } from "../context/AuthContext";
+
+
+const BASE_URL = "https://nice-teal-worm-tutu.cyclic.app";
+
+export async function signup(data) {
+    try {
+        const response = await axios.post(`${BASE_URL}/signup`, data);
+        return response;
+    } catch (error) {
+        // Trate o erro aqui, por exemplo, lançando novamente para que seja tratado em outro lugar ou registrando no console.
+        console.error("Erro na solicitação de inscrição:", error);
+        throw error;
+    }
+}
+
+export async function signin(data){
+    try {
+        const response = await axios.post(`${BASE_URL}/signin`, data);
+        const token = response.data;
+        return token;
+
+    } catch (error) {
+        console.error("Erro ao fazer login:", error);
+        throw error; // Rejeitar a promessa para que o erro seja capturado no bloco catch onde a função foi chamada
+    }
+}
+
+export async function userLogged(token) {
+    try {
+        const response = await axios.get(`${BASE_URL}/me`, {
+            headers: { Authorization: `Bearer ${token}` },
+        });
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+}
